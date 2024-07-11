4b9c5d51e2aaa0dd7dddb0b6fb310204c65bf2db
Sudheeksha-Herle-V <151427324+Sudheeksha-Herle-V@users.noreply.github.com>
2024-07-11 20:47:20 +0530

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://sudheekshaherle:Sudhee07@cluster0.5hryqqh.mongodb.net/?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
